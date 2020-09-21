8ca342be7751286beb23c3adc3c936c23f05171b
JordanMicle <JordanMicle2019@gmail.com>
2020-09-22 01:13:46 +0800

    fixed for asset type

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 $pdk.controller.addEventListener("OnReleasePreview", function(payload) {
-    setBackgroundImage(payload.data.url);
+    setBackgroundImage(payload.data.mediaPID, payload.data.url);
 });
 
-function setBackgroundImage(videoUrl) {
-    document.getElementsByClassName("tpVideo")[0].style.backgroundImage = "url(" + videoUrl + "?assetTypes=Poster)";
+function setBackgroundImage(medaiPid, videoUrl) {
+    var baseURL = "https://link.theplatform.com/s/ifSiAC/media/"
+    document.getElementsByClassName("tpVideo")[0].style.backgroundImage = "url(" + baseURL + medaiPid + "?assetTypes=Poster)";
     document.getElementsByClassName("tpVideo")[0].style.backgroundSize = "cover";
 }
