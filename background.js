4e2fd7714fb5182cc44ed5cc7853c4f8a0992850
JordanMicle <JordanMicle2019@gmail.com>
2020-09-22 01:18:59 +0800

    fixed for asset type

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ $pdk.controller.addEventListener("OnReleasePreview", function(payload) {
 });
 
 function setBackgroundImage(medaiPid, videoUrl) {
-    var baseURL = "https://link.theplatform.com/s/ifSiAC/media/"
+    var baseURL = "https://link.theplatform.com/s/IfSiAC/media/"
     document.getElementsByClassName("tpVideo")[0].style.backgroundImage = "url(" + baseURL + medaiPid + "?assetTypes=Poster)";
     document.getElementsByClassName("tpVideo")[0].style.backgroundSize = "cover";
 }
